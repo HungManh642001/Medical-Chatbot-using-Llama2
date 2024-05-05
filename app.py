0c4274baaa7197b06d654e1b75e54a212e8b5e5a
HungManh642001 <hungmanh642001@gmail.com>
2024-05-05 11:19:21 +0700

    updated

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
+from flask import Flask, render_template, jsonify, request
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
-from langchain.vectorstores import Pinecone
+from langchain_community.vectorstores import Pinecone
 import pinecone
 from langchain.prompts import PromptTemplate
-from langchain.llms import CTransformers
+from langchain_community.llms import CTransformers
 
 from dotenv import load_dotenv
 import os
-
-from src.helper import load_pdf, text_split, download_hugging_face_embeddings
+from src.helper import download_hugging_face_embeddings
 from src.prompt import *
+
+
+app = Flask(__name__)
+
 
 load_dotenv()
 
@@ -17,20 +21,20 @@ PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
 index_name = 'medical-chatbot'
 
-extracted_data = load_pdf('../data/')
-
-text_chunks = text_split(extracted_data)
-
+# Download embeddings
 embeddings = download_hugging_face_embeddings()
 
+# Connect to pinecone
 pinecone.Pinecone(api_key=PINECONE_API_KEY)
 
+# Loading the index
 vector_db = Pinecone.from_existing_index(index_name, embeddings)
+
 
 PROMPT = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
 chain_type_kwargs = {'prompt': PROMPT}
 
-llm = CTransformers(model='../model/llama-2-7b-chat.ggmlv3.q4_0.bin', 
+llm = CTransformers(model='model/llama-2-7b-chat.ggmlv3.q2_K.bin', 
                     model_type='llama',
                     config={'max_new_tokens': 512,
                             'temperature': 0.8})
@@ -42,3 +46,22 @@ qa = RetrievalQA.from_chain_type(
     return_source_documents=True,
     chain_type_kwargs=chain_type_kwargs
 )
+
+# Build app
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route('/get', methods=['GET', 'POST'])
+def chat():
+    msg = request.form['msg']
+    input = msg
+    print(input)
+    result = qa({'query': input})
+    print('Response: ', result['result'])
+    return str(result['result'])
+
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
